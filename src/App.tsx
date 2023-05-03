809315008e307151d9f82d0ec08f3a41a294ea3b
Alex Gustafsson <alex-gustafsson@live.se>
2023-05-03 11:39:54 +0200

    Store pressed keys for typing codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
 import TouchSurface from "./TouchSurface";
 
 export default function (): JSX.Element {
+  const [queue, setQueue] = useState<string[]>([]);
+
+  useEffect(() => {
+    document.addEventListener("keyup", (event) => {
+      const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz ";
+      if (!alphabet.includes(event.key)) {
+        return;
+      }
+
+      setQueue((queue) => [...queue, event.key]);
+    });
+  }, []);
+
   return (
     <>
       <header className="fixed w-full p-2 flex justify-center bg-white drop-shadow-sm">
@@ -9,6 +23,9 @@ export default function (): JSX.Element {
       <main className="flex-1 mt-20">
         <TouchSurface className="w-full h-full" />
       </main>
+      <footer className="fixed w-full bottom-0 flex justify-center">
+        <p className="text-xl">{queue.join("")}</p>
+      </footer>
     </>
   );
 }
